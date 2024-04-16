cd575d9cac5cf29822dc139222177d4e0e4d5e27
Caesar Luis II M. Claridad <sesar091913@gmail.com>
2024-04-16 22:50:45 +0800

    added frontend origin in server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({ origin:'http://localhost:5173'}))
+app.use(cors({ origin:'https://ccat-blogs-lost-found-frontend.onrender.com'}))
 
 //middleware
 app.use(express.json());
