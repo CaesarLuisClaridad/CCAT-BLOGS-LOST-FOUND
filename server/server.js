8524facc772fd65248daeb2cd50e6748db985be6
Caesar Luis II M. Claridad <sesar091913@gmail.com>
2024-04-17 10:12:39 +0800

    dist

--- server/server.js
+++ server/server.js
@@ -31,11 +31,14 @@ app.use("/api", blogRoutes);
 app.use("/item", lostandFoundRoutes);
 app.use("/notif", notificationsRoutes);
 
+
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 
 
