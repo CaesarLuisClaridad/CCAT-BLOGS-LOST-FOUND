50c329f8043abef84ab5f4f65b6ab5653b47a53a
Caesar Luis II M. Claridad <sesar091913@gmail.com>
2024-04-29 09:00:48 +0800

    fixing bug

--- server/server.js
+++ server/server.js
@@ -33,11 +33,11 @@ app.use("/notif", notificationsRoutes);
 
 
 // Serve static files from the client/dist directory
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 // Serve the index.html file for all routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
 });
 
 // app.get("*", (req, res) => {
